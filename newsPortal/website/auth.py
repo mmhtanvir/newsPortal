37d32270ebc1e45d0d1c5d7ce2e3cc9744cd06db
MH  Tanvir <82567711+mmhtanvir@users.noreply.github.com>
2024-08-23 08:10:46 +0600

    Add files via upload

--- newsPortal/website/auth.py
+++ newsPortal/website/auth.py
@@ -1,25 +1,69 @@
-from flask import Blueprint, render_template, redirect, url_for, request
+from flask import Blueprint, render_template, redirect, url_for, request, flash
+from . import db
+from .models import User 
+from flask_login import login_user, logout_user, login_required, current_user
+from werkzeug.security import generate_password_hash, check_password_hash
 
 auth = Blueprint("auth", __name__)
 from flask import Blueprint
 
 auth = Blueprint("auth", __name__)
 
-@auth.route("/login")
+@auth.route("/login", methods=['GET', 'POST'])
 def login():
-    email = request.form.get("email")
-    password = request.form.get("password")
+    if request.method == 'POST':
+
+        email = request.form.get("email")
+        password = request.form.get("password")
+
+        user = User.query.filter_by(email=email).first()
+        if user:
+            if check_password_hash(user.password, password):
+                flash('Logged in!', category='success')
+                login_user(user, remember=True)
+                return redirect(url_for('views.home'))
+            else:
+                flash('Incorrect Password', category='error')
+        else:
+            flash('Email does not exist', category='error')
+
+
+
     return render_template("login.html")
 
-
-@auth.route("/signup")
+@auth.route("/signup", methods=['GET', 'POST'])
 def signup():
-    name = request.form.get("name")
-    email = request.form.get("email")
-    password = request.form.get("password")
-    password2 = request.form.get("password2")
+    if request.method == 'POST':
+
+        name = request.form.get("name")
+        email = request.form.get("email")
+        password = request.form.get("password")
+        password2 = request.form.get("password2")
+
+        email_exists = User.query.filter_by(email=email).first()
+
+        if email_exists:
+            flash('Email already in use', category='error')
+        elif password != password2:
+            flash('Passwords don\'t match', category='error')
+        elif len(name) < 2:
+            flash('Name to short', category='error')
+        elif len(email) < 4:
+            flash('Invalid Email', category='error')
+        elif len(password) < 8:
+            flash('Password too short', category='error')
+        else:
+            new_user = User(email=email, name=name, password=generate_password_hash(password, method='scrypt'))
+            db.session.add(new_user)
+            db.session.commit()
+            login_user(new_user, remember=True)
+            flash('User created')
+            return redirect(url_for('views.home'))
+
     return render_template("signup.html")
 
 @auth.route("/logout")
+@login_required
 def logout():
+    logout_user()
     return redirect(url_for("views.home"))
---
+++ newsPortal/website/models.py
@@ -0,0 +1,10 @@
+from . import db
+from flask_login import UserMixin
+from sqlalchemy.sql import func
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), nullable=False)
+    email = db.Column(db.String(255), unique=True)
+    password = db.Column(db.String(255))
+    date_created = db.Column(db.DateTime(timezone=True), default=func.now())
